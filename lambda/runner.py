51569b485e350268c4fdd430c2ed23ab60170b38
outcastgeek <outcastgeek+git@gmail.com>
2017-07-11 16:46:07 -0500

    Implementing Basic Worker in Go + Setup GraphQL in it

--- lambda/runner.py
+++ lambda/runner.py
@@ -5,6 +5,20 @@ respString = executeQuery("{hello}")
 
 print("Cool Response: ", respString)
 
-weatherString = executeQuery("{weather}")
+weatherString = executeQuery(
+    """
+    {
+        weather(location: "lambert") {
+            pressure
+            humidity
+            speed
+            deg
+            location
+            description
+            temp
+          }
+    }
+    """
+)
 
 print("Weather Response: ", weatherString)
