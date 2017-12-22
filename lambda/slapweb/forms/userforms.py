df8b5faae28044e7b9f641f6eb0c46c021764552
outcastgeek <outcastgeek+git@gmail.com>
2017-12-21 19:08:40 -0600

    Implemented Dual form for Register / Login

--- lambda/slapweb/forms/userforms.py
+++ lambda/slapweb/forms/userforms.py
@@ -5,6 +5,8 @@ import deform
 
 from pyramid.renderers import render
 
+# Schemas
+
 class UserRegistrationSchema(deform.schema.CSRFSchema):
     email = colander.SchemaNode(
         colander.String(allow_empty=False),
@@ -20,14 +22,42 @@ class UserRegistrationSchema(deform.schema.CSRFSchema):
         widget=deform.widget.CheckedPasswordWidget(redisplay=True)
     )
 
+class UserLoginSchema(deform.schema.CSRFSchema):
+    email = colander.SchemaNode(
+        colander.String(allow_empty=False),
+        validator=colander.Email()
+    )
+    password = colander.SchemaNode(
+        colander.String(),
+        validator=colander.Length(min=8),
+        widget=deform.widget.PasswordWidget(redisplay=True)
+    )
+
+# Helper Methods
+
 def get_user_registration_form(
         request,
-        button_name='submit',
+        button_name='register',
         use_ajax=True
 ):
     schema = UserRegistrationSchema().bind(request=request)
     form = deform.Form(
         schema,
+        action=request.route_url('register'),
+        use_ajax=use_ajax,
+        buttons=(button_name,)
+    )
+    return form
+
+def get_user_login_form(
+        request,
+        button_name='login',
+        use_ajax=True
+):
+    schema = UserLoginSchema().bind(request=request)
+    form = deform.Form(
+        schema,
+        action=request.route_url('login'),
         use_ajax=use_ajax,
         buttons=(button_name,)
     )
--- lambda/slapweb/views/auth_views.py
+++ lambda/slapweb/views/auth_views.py
@@ -3,17 +3,22 @@ import logging
 
 try:
     from slapweb.forms.userforms import (
+        get_user_login_form,
         get_user_registration_form
     )
 except:
     from forms.userforms import (
+        get_user_login_form,
         get_user_registration_form
     )
 
 import colander
 import deform
 
-from pyramid.view import view_config
+from pyramid.view import (
+    view_config,
+    view_defaults
+)
 
 from pyramid.httpexceptions import HTTPFound
 
@@ -28,38 +33,43 @@ from pyramid.security import (
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
-@view_config(route_name='login', renderer='templates/login.jinja2')
-@view_config(route_name='register', renderer='templates/login.jinja2')
-@forbidden_view_config(renderer='templates/login.jinja2')
-def login(request):
-    login_route_url = request.route_url('login')
-    register_route_url = request.route_url('register')
-    referrer = request.url
-    log.debug(f"Referrer: {referrer}")
-    session = request.session
-    if referrer == login_route_url or referrer == register_route_url:
-        home_route_url = request.route_url('home')
-        referrer = home_route_url # never use the login from itself as came_from
-        # referrer = '/' # never use the login from itself as came_from
-    else:
-        session.flash("You lack the credentials")
-    came_from = request.route_url('home')
-    # came_from = request.params.get('came_from', referrer)
-    session['came_from'] = came_from
-    log.debug(f"Came from: {came_from}")
-    form = get_user_registration_form(request)
-    rendered_form = None
-    if 'submit' in request.POST:
+@view_defaults(renderer='templates/login.jinja2')
+class AuthViews:
+    def __init__(self, request):
+
+        self.request = request
+        self.session = request.session
+        self.registration_form = get_user_registration_form(request)
+        self.login_form = get_user_login_form(request)
+
+        login_route_url = request.route_url('login')
+        register_route_url = request.route_url('register')
+        referrer = request.url
+        log.debug(f"Referrer: {referrer}")
+        if referrer == login_route_url or referrer == register_route_url:
+            home_route_url = request.route_url('home')
+            referrer = home_route_url # never use the login from itself as came_from
+            # referrer = '/' # never use the login from itself as came_from
+        else:
+            self.session.flash("You lack the credentials")
+
+        self.came_from = request.route_url('home')
+        # self.came_from = request.params.get('came_from', referrer) # TODO: Revisit this!!!!
+        self.session['came_from'] = self.came_from
+        log.debug(f"Came from: {self.came_from}")
+
+    def process_form(self, form):
+        rendered_form = None
         try:
-            form_data = form.validate(request.POST.items())
+            form_data = form.validate(self.request.POST.items())
             log.debug(f"User Data: {form_data}")
             username = form_data['username']
             email = form_data['email']
-            session.flash(f"Welcome {username}!")
-            headers = remember(request, email)
-            log.info(f"Successfully registered: {username}")
+            self.session.flash(f"Welcome {username}!")
+            headers = remember(self.request, email)
+            log.info(f"Processed successfully for: {username}")
             return HTTPFound(
-                location=came_from,
+                location=self.came_from,
                 headers=headers
             )
         except deform.ValidationFailure as e:
@@ -73,10 +83,28 @@ def login(request):
             # Do a form level error message
             exc = colander.Invalid(form.widget, f"Ouch!! {e}")
             form.widget.handle_error(form, exc)
+        if rendered_form is None:
+            rendered_form = form.render()
+        return rendered_form
 
-    if rendered_form is None:
-        rendered_form = form.render()
+    @view_config(route_name='login', request_method='GET')
+    @view_config(route_name='register', request_method='GET')
+    def forms_display(self):
+        return {
+            'registration_form': self.registration_form.render(),
+            'login_form': self.login_form.render()
+        }
 
-    return {
-        'form': rendered_form
-    }
+    @view_config(route_name='register', request_method='POST')
+    def handle_registration(self):
+        return {
+            'registration_form': self.process_form(self.registration_form),
+            'login_form': self.login_form.render()
+        }
+
+    @view_config(route_name='login', request_method='POST')
+    def handle_login(self):
+        return {
+            'registration_form': self.registration_form.render(),
+            'login_form': self.process_form(self.login_form)
+        }
