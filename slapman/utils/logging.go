339281f90dcabedbc0a907a22e1f517557914bd8
outcastgeek <outcastgeek+git@gmail.com>
2018-02-07 13:08:47 -0600

    Updated Deps + Logging for Go GraphQL Lambda Function

--- slapman/utils/logging.go
+++ slapman/utils/logging.go
@@ -4,24 +4,39 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 // Check this out: http://www.goinggo.net/2013/11/using-log-package-in-go.html
 // Check this out: https://gist.github.com/alphazero/2718939
 
 const (
-	delim  = " - "
-	FATAL  = "[FATAL]"
-	ERROR  = "[ERROR]"
-	WARN   = "[WARN ]"
-	DEBUG  = "[DEBUG]"
-	INFO   = "[INFO ]"
+	delim = " - "
+
+	FATAL = "[FATAL]"
+	ERROR = "[ERROR]"
+	WARN  = "[WARN ]"
+	DEBUG = "[DEBUG]"
+	INFO  = "[INFO ]"
+
 	levlen = len(INFO)
 	len2   = len(delim) + levlen
 	len3   = 2*len(delim) + levlen
+
+	prefix = "[dbu][%s]"
 )
 
-const prefix = "[slapman][%s]"
+var logLevels map[string]int
+
+func init() {
+	logLevels = map[string]int{
+		"FATAL": 1,
+		"ERROR": 2,
+		"INFO":  3,
+		"WARN":  4,
+		"DEBUG": 5,
+	}
+}
 
 type Logger struct {
 	Logger *log.Logger
@@ -53,12 +68,24 @@ func join3(level, meta, msg string) string {
 	return string(j)
 }
 
+func (logger *Logger) output(level int, msg string) {
+	logLevel := 2 // Error Level By Default
+	envLogLevel := os.Getenv("LOG_LEVEL")
+	if val, ok := logLevels[strings.ToUpper(envLogLevel)]; ok {
+		logLevel = val
+	}
+	//fmt.Printf("LOG LEVEL: %s==%d\n", envLogLevel, logLevel)
+	if level <= logLevel {
+		logger.Logger.Println(msg)
+	}
+}
+
 // NOTE - the semantics here are different from go's logger.Fatal
 // It will neither panic nor exit
 
 // Fatal Logs Fatal
 func (logger *Logger) Fatal(meta string, e error) {
-	logger.Logger.Println(join3(FATAL, meta, e.Error()))
+	logger.output(1, join3(FATAL, meta, e.Error()))
 }
 
 // NOTE - the semantics here are different from go's logger.Logger.Fatal
@@ -66,45 +93,45 @@ func (logger *Logger) Fatal(meta string, e error) {
 
 // Fatalf Logs Fatal and Format
 func (logger *Logger) Fatalf(format string, v ...interface{}) {
-	logger.Logger.Println(join2(FATAL, fmt.Sprintf(format, v...)))
+	logger.output(1, join2(FATAL, fmt.Sprintf(format, v...)))
 }
 
 // Error Logs Error
 func (logger *Logger) Error(meta string, e error) {
-	logger.Logger.Println(join3(ERROR, meta, e.Error()))
+	logger.output(2, join3(ERROR, meta, e.Error()))
 }
 
 // Errorf Logs Error and Format
 func (logger *Logger) Errorf(format string, v ...interface{}) {
-	logger.Logger.Println(join2(ERROR, fmt.Sprintf(format, v...)))
+	logger.output(2, join2(ERROR, fmt.Sprintf(format, v...)))
 }
 
 // Debug Logs Debug
 func (logger *Logger) Debug(msg string) {
-	logger.Logger.Println(join2(DEBUG, msg))
+	logger.output(5, join2(DEBUG, msg))
 }
 
 // Debugf Logs Debug and Format
 func (logger *Logger) Debugf(format string, v ...interface{}) {
-	logger.Logger.Println(join2(DEBUG, fmt.Sprintf(format, v...)))
+	logger.output(5, join2(DEBUG, fmt.Sprintf(format, v...)))
 }
 
 // Warn Logs Warn
 func (logger *Logger) Warn(msg string) {
-	logger.Logger.Println(join2(WARN, msg))
+	logger.output(4, join2(WARN, msg))
 }
 
 // Warnf Logs Warn and Format
 func (logger *Logger) Warnf(format string, v ...interface{}) {
-	logger.Logger.Println(join2(WARN, fmt.Sprintf(format, v...)))
+	logger.output(4, join2(WARN, fmt.Sprintf(format, v...)))
 }
 
 // Info Logs Info
 func (logger *Logger) Info(msg string) {
-	logger.Logger.Println(join2(INFO, msg))
+	logger.output(3, join2(INFO, msg))
 }
 
 // Infof Logs Info and Format
 func (logger *Logger) Infof(format string, v ...interface{}) {
-	logger.Logger.Println(join2(INFO, fmt.Sprintf(format, v...)))
+	logger.output(3, join2(INFO, fmt.Sprintf(format, v...)))
 }
