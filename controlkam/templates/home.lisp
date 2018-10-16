c87ce06e8f78eb8ddc45b25cd7155238bfda7af2
outcastgeek <outcastgeek+git@gmail.com>
2018-10-16 17:53:47 -0500

    ControlKam + Bootstrap CSS

--- controlkam/templates/home.lisp
+++ controlkam/templates/home.lisp
@@ -2,10 +2,16 @@
 (PROGN
  (ECO-TEMPLATE:DEFTEMPLATE ECO-TEMPLATE::HOME (ECO-TEMPLATE::TITLE) NIL
                            (WRITE-STRING "
-  <h1>"
+<div class=\"card\" style=\"width: 18rem;\">
+  <img class=\"card-img-top\" src=\"...\" alt=\"Card image cap\">
+  <div class=\"card-body\">
+    <h5 class=\"card-title\">"
                                          ECO-TEMPLATE::ECO-STREAM)
                            (WRITE-STRING (ECO-TEMPLATE:E ECO-TEMPLATE::TITLE) ECO-TEMPLATE::ECO-STREAM)
-                           (WRITE-STRING "</h1>
-  <p>Hello World!</p>
+                           (WRITE-STRING "</h5>
+    <p class=\"card-text\">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href=\"/hello\" class=\"btn btn-primary\">Go somewhere</a>
+  </div>
+</div>
 "
                                          ECO-TEMPLATE::ECO-STREAM))) 
